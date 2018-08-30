ecc179a6d3c84cb2d97edad6e9090a63a267a8a1
xu_xi <xu_xi@worksap.co.jp>
2018-08-30 17:53:30 +0800

    V1

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -4,8 +4,9 @@ create table "user" (
   "password" varchar(255) not null,
   "role" varchar(255),
   "username" varchar(255) not null unique,
-  "resume_id" varchar(255)
+  "resume_id" varchar(255),
+  "company_id" varchar(255)
 );
 
 -- add default administrator with username [admin] and password [123456]
-insert into "user" values ('1', 'admin@admin.com', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi ', 'admin', 'admin', null);
+insert into "user" values ('1', 'admin@admin.com', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi ', 'admin', 'admin', null,null);
