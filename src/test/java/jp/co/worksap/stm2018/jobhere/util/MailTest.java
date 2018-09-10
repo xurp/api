dfe3d7b25049513d6927bb0f2f195873543e2e49
zhai_g <zhai_g@worksap.co.jp>
2018-09-10 12:54:19 +0800

    #65 Send mail with new thread

--- src/test/java/jp/co/worksap/stm2018/jobhere/util/MailTest.java
+++ src/test/java/jp/co/worksap/stm2018/jobhere/util/MailTest.java
@@ -3,11 +3,22 @@ package jp.co.worksap.stm2018.jobhere.util;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class MailTest {
 
     @Test
     @Ignore
     public void sendTest() {
-        Mail.send("chorespore@163.com","chorespore@163.com","Test mail to myself2","Test mail to myself!!!2");
+
+        Mail.send("chorespore@163.com", "chorespore@163.com", "Test mail to myself using new Thread!", "Test mail to myself!!!");
+        Mail.send("chorespore@163.com", "chorespore@163.com", "Test mail to myself using new Thread!2", "Test mail to myself!!!2");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("finished!");
     }
 }
