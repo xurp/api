b31ed3507aab24e9d3a30c43dede762c00ac756a
xu_xi <xu_xi@worksap.co.jp>
2018-09-11 17:40:19 +0800

    #79 Send choosing date email to candidate and open choosing page

--- src/main/java/jp/co/worksap/stm2018/jobhere/dao/OutboxRepository.java
+++ src/main/java/jp/co/worksap/stm2018/jobhere/dao/OutboxRepository.java
@@ -5,8 +5,10 @@ import jp.co.worksap.stm2018.jobhere.model.domain.Outbox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
 public interface OutboxRepository extends JpaRepository<Outbox, String> {
+    List<Outbox> findByOperationId(String id);
 }
--- src/main/java/jp/co/worksap/stm2018/jobhere/service/impl/AppointedTimeServiceImpl.java
+++ src/main/java/jp/co/worksap/stm2018/jobhere/service/impl/AppointedTimeServiceImpl.java
@@ -1,20 +1,38 @@
 package jp.co.worksap.stm2018.jobhere.service.impl;
 
+import jp.co.worksap.stm2018.jobhere.dao.ApplicationRepository;
 import jp.co.worksap.stm2018.jobhere.dao.AppointedTimeRepository;
+import jp.co.worksap.stm2018.jobhere.dao.AssessmentRepository;
+import jp.co.worksap.stm2018.jobhere.dao.OutboxRepository;
 import jp.co.worksap.stm2018.jobhere.http.ValidationException;
+import jp.co.worksap.stm2018.jobhere.model.domain.Application;
 import jp.co.worksap.stm2018.jobhere.model.domain.AppointedTime;
+import jp.co.worksap.stm2018.jobhere.model.domain.Assessment;
+import jp.co.worksap.stm2018.jobhere.model.domain.Outbox;
 import jp.co.worksap.stm2018.jobhere.model.dto.request.AppointedTimeDTO;
+import jp.co.worksap.stm2018.jobhere.model.dto.response.ChooseDateDTO;
 import jp.co.worksap.stm2018.jobhere.service.AppointedTimeService;
+import jp.co.worksap.stm2018.jobhere.util.Mail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class AppointedTimeServiceImpl implements AppointedTimeService {
 
     @Autowired
     private AppointedTimeRepository appointedTimeRepository;
+    @Autowired
+    private OutboxRepository outboxRepository;
+    @Autowired
+    private ApplicationRepository applicationRepository;
+    @Autowired
+    private AssessmentRepository assessmentRepository;
 
 
     @Override
@@ -27,13 +45,43 @@ public class AppointedTimeServiceImpl implements AppointedTimeService {
     }
 
     @Override
-    public void update(AppointedTimeDTO appointedTimeDTO) {
+    public void update(AppointedTimeDTO appointedTimeDTO,String path) {
         List<AppointedTime> appointedTimeList = appointedTimeRepository
                 .getByOperationIdAndCooperatorId(appointedTimeDTO.getOperationId(), appointedTimeDTO.getCooperatorId());
-
+        List<AppointedTime> appointedTimeRemainList = appointedTimeRepository
+                .getByOperationId(appointedTimeDTO.getOperationId());
         for (int i = 0; i < appointedTimeList.size(); i++) {
             appointedTimeList.get(i).setStartTime(appointedTimeDTO.getStartTimes().get(i));
             appointedTimeRepository.save(appointedTimeList.get(i));
         }
+        if(appointedTimeList.size()==appointedTimeRemainList.size()){
+            List<Outbox> outboxList=outboxRepository.findByOperationId(appointedTimeDTO.getOperationId());
+            if(outboxList!=null&&outboxList.size()>0){
+                for(Outbox outbox:outboxList){
+                    String applicationId=outbox.getApplicationId();
+                    Application application=applicationRepository.findById(applicationId).get();
+                    String email=application.getResume().getEmail();
+                    //now assessId is needed, but appointedTime only has applicationId
+                    //return the newest assessment in this application
+                    List<Assessment> assessmentList=assessmentRepository.findByApplicationId(applicationId);
+                    List<Assessment> sortedList= assessmentList.stream().sorted((a, b) -> Double.compare(Double.parseDouble(a.getStep()),Double.parseDouble(b.getStep()))).collect(Collectors.toList());
+                    String content="Please click the link to choose your interview time:  "+path+"/#/schedule/candidate/"+appointedTimeDTO.getOperationId()+"/"+sortedList.get(sortedList.size()-1).getId();
+                    Mail.send("chorespore@163.com", email, "["+application.getJob().getCompany()+"] Please choose your interview time",content);
+                }
+            }
+
+        }
+
+    }
+
+    @Override
+    public Set<ChooseDateDTO> getCandidateDate(String id) {
+        List<AppointedTime> appointedTimeList=appointedTimeRepository.getByOperationId(id);
+        Set<ChooseDateDTO> chooseDateDTOList=new HashSet<>();
+        for(AppointedTime appointedTime:appointedTimeList){
+            //starttime is different
+            chooseDateDTOList.add(ChooseDateDTO.builder().startTime(appointedTime.getStartTime()).build());
+        }
+        return chooseDateDTOList;
     }
 }
