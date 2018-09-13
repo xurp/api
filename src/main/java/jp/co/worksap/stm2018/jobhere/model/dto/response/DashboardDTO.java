c39fbc2021cdf0459401a71518e8ee63459ec96a
zhai_g <zhai_g@worksap.co.jp>
2018-09-13 12:02:23 +0800

    Get dashboard data

---
+++ src/main/java/jp/co/worksap/stm2018/jobhere/model/dto/response/DashboardDTO.java
@@ -0,0 +1,19 @@
+package jp.co.worksap.stm2018.jobhere.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DashboardDTO {
+    private int jobCnt;
+    private int candidateCnt;
+    private int hrCnt;
+    private List<String> deptList;
+}
